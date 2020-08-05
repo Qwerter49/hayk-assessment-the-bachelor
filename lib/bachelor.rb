8f88d79cdd7eba86d03ee5c974b840789e557b02
Jay Oliver <jaykamas@gmail.com>
2020-08-05 15:30:27 -0400

    simplified methods

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,51 +1,39 @@
 require 'pry'
 
+#creates aoh of contestants
+def all_contestants data 
+  total_contestants = []
+  data.each do |season, info|
+    info.each do |person|
+      total_contestants << person
+    end
+  end
+  total_contestants
+end
 
 #based on given season, returns first name of `winner`
 def get_first_name_of_season_winner(data, season)
-  data[season].find{|person| person["status"] == "Winner"}["name"].partition(" ").first   
+  data[season].find{|person| person["status"] == "Winner"}["name"].split(" ").first   
 end
 
-#finds person who has certian occupation
-def get_contestant_name(data, occupation)
-  data.each do |season, info|
-    if  info.find{|person| person["occupation"] == occupation}
-      return info.find{|person| person["occupation"] == occupation}["name"]
-    end
-  end
+#finds a contestants name given their occupation
+def get_contestant_name data, occupation
+  all_contestants(data).find{|person| person["occupation"] == occupation}["name"]
 end
 
-#returns a counter of number of contestants from a hometown
-def count_contestants_by_hometown(data, hometown)
-  counter = 0
-  data.each do |season, info|
-    info.each do |person| 
-      if person["hometown"] == hometown
-        counter += 1
-      end 
-    end
-  end
-  counter
+#returns the number of contestants from a given hometown
+def count_contestants_by_hometown data, hometown
+  all_contestants(data).count{|person| person["hometown"] == hometown}
 end
 
-#Returns first contestant from a given hometown
-def get_occupation(data, hometown)
-  data.each do |season, info|
-    info.find do |person|
-      if person["hometown"] == hometown
-        return person["occupation"]
-      end
-    end
-  end
+#returns the occupation of the first contestant fro a given hometown
+def get_occupation data, hometown
+  all_contestants(data).find{|person| person["hometown"] == hometown}["occupation"]
 end
 
 #given a season, finds the average age of all contestants
-def get_average_age_for_season(data, season)
-  all_ages = []
-  data[season].each do |person|
-    all_ages << person["age"].to_f
-  end
-  average_age = all_ages.reduce(0) {|sum, n| sum + n}.to_f/all_ages.length
-  average_age.round
+def get_average_age_for_season data, season 
+  (data[season].map{|person| person["age"].to_f}.reduce{|sum, n| sum + n} /data[season].length).round
 end
+
 
